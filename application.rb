0af74e5d6ac4227a4e716039b0e5e2101bff862d
Phillip Longman <himself@philliplongman.com>
2016-08-10 21:18:09 -0400

    Tweak asset-pipeline setting.

--- application.rb
+++ application.rb
@@ -10,6 +10,7 @@ class PivotalTestimony < Sinatra::Base
   end
 
   configure do
+    set :assets_precompile, %w(application.js application.css *.png *.jpg *.svg *.eot *.ttf *.woff)
     register Sinatra::AssetPipeline
 
     if defined?(RailsAssets)
